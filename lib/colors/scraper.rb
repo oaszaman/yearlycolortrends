a82e3f8d662c1f66650a28c2050428a55fb9382f
Shahzab A Zaman <o.aszaman00@gmail.com>
2021-11-10 18:19:28 -0600

    created scrape_years in scraper.rb

--- lib/colors/scraper.rb
+++ lib/colors/scraper.rb
@@ -1,3 +1,7 @@
 class Colors::Scraper
 
+    def scrape_years
+        doc = Nokogiri::HTML(open("https://www.pantone.com/color-of-the-year-2021"))
+    end
+
 end
--- lib/colors/years.rb
+++ lib/colors/years.rb
@@ -1,14 +1,18 @@
 class Colors::Years
 
-    @@all = ["2021", "2020", "2019", "2018"]
+    @@all = []
     attr_accessor :holder
 
     def initialize(holder)
         @holder = holder 
+        save 
     end
 
     def self.all
         @@all 
     end
 
+    def save 
+        @@all << self
+    end
 end
